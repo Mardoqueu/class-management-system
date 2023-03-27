1a0a32f2c6ae8e23593b1ee977d0ea5a521b73d4
Mardoqueu Sousa <mdq767@gmail.com>
2023-03-27 11:25:28 -0300

    getting started

---
+++ src/test/java/com/mardoqueu/classmanagementsystem/ClassManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mardoqueu.classmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
